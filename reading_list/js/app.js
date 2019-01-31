190a71b7429248b26f79fab356bd0d8d155f974b
Conor <cwilson0005@hotmail.co.uk>
2019-01-31 14:30:57 +0000

    progress

--- reading_list/js/app.js
+++ reading_list/js/app.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-})
+
+  const form = document.querySelector('#new-item-form');
+  console.dir(form);
+  form.addEventListener('submit', handleSubmit);
+
+  const list = document.querySelector('#reading-list');
+  list.appendChild(newParaItem);
+});
+
+const handleSubmit = function () {
+  event.preventDefault();
+  // const newParaItem = document.createElement('p');
+  // newParaItem.textContent = `Title: ${this.title.value} | Author: ${this.author.value} | Category: ${this.category.value}`;
+  // newParaItem.classList.add('recorded-item');
+  const submitText = document.querySelector('#reading-list');
+  submitText.textContent = `Title: ${this.title.value} | Author: ${this.author.value} | Category: ${this.category.value}`;
+};
